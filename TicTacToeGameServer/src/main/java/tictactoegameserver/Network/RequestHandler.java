359270d40b1ff1e50d2921e4a78503b14f377151
Ayman Mohamed Hosny Kheir-Eldeen <Aymanmido067@gmail.com>
2022-02-12 17:53:38 +0200

    some more configurations

--- TicTacToeGameServer/src/main/java/tictactoegameserver/Network/RequestHandler.java
+++ TicTacToeGameServer/src/main/java/tictactoegameserver/Network/RequestHandler.java
@@ -12,7 +12,8 @@ import org.json.simple.JSONValue;
 import tictactoegameserver.Database.DatabaseManager;
 import tictactoegameserver.Database.Entities.Enums.MappingFunctions;
 import tictactoegameserver.Database.Entities.Player;
-import static tictactoegameserver.Network.PlayerHandler.sendUpdateOnlinePlayersResponse;
+import static tictactoegameserver.Network.PlayerHandler.*;
+import static tictactoegameserver.Network.ResponseCreator.*;
 
 /**
  *
@@ -44,97 +45,29 @@ public class RequestHandler {
 
         DatabaseManager.openDataBaseConnection();
         if (!DatabaseManager.isPlayerExist(userName)) {
-            return ResponseCreator.playerNotExistResponse();
+            return playerNotExistResponse();
         }
         if (!DatabaseManager.isValidPlayer(userName, password)) {
-            return ResponseCreator.wrongPasswordResponse();
+            return wrongPasswordResponse();
         }
         playerHandler.player = DatabaseManager.getPlayer(userName);
         DatabaseManager.togglePlayerStatus(playerHandler.player);
         DatabaseManager.closeDataBaseConnection();
         sendUpdateOnlinePlayersResponse();
-        return ResponseCreator.loginSuccessResponse(userName, playerHandler.player);
+        return loginSuccessResponse(userName, playerHandler.player);
     }
     
     public static String handleRegister(JSONObject data) {
 
         String userName = (String) data.get("username");
         String password = (String) data.get("password");
-        JSONObject responseObject = new JSONObject();
         DatabaseManager.openDataBaseConnection();
         if (DatabaseManager.isPlayerExist(userName)) {
-            responseObject.put("response", "player exists");
-            return JSONValue.toJSONString(responseObject);
+            return playerExistResponse();
         } else {
             DatabaseManager.addNewPlayer(userName, password);
-            responseObject.put("response", "reqister sucsess");
         }
         DatabaseManager.closeDataBaseConnection();
-        return JSONValue.toJSONString(responseObject);
-    }
-
-    public static JSONObject getPlayerJsonObject(String userName, Player player) {
-        
-        JSONObject dataObject = new JSONObject();
-        
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formatedDateTime = player.getRegisterDate().format(formatter);
-        dataObject.put("userName", player.getUserName());
-        dataObject.put("bonusPoints", player.getBonusPoints());
-        dataObject.put("playerRank", MappingFunctions.mapPlayerRank(player.getPlayerRank()));
-        dataObject.put("registerDate", formatedDateTime);
-        
-        return dataObject;
-    }
-    
-    public static JSONObject getOnlinePlayersJsonObject() {
-        ArrayList<Player> onlinePlayers = null;
-        
-        
-        DatabaseManager.openDataBaseConnection();
-        onlinePlayers = DatabaseManager.getOnlinePlayers();
-        DatabaseManager.closeDataBaseConnection();
-        
-        JSONArray onlinePlayersNames = new JSONArray();
-        for (var player : onlinePlayers) {
-            onlinePlayersNames.add(player.getUserName());
-        }
-        JSONObject dataObject = new JSONObject();
-        dataObject.put("onlinePlayers", onlinePlayersNames);
-        
-        return dataObject;
-    }
-
-    public static void tryJson() {
-
-        // Step 1: prepare the jsonObject that you want to send
-        // as a request or as a response that consists of action and data
-        JSONObject data = new JSONObject();
-        data.put("username", "ayman");
-        data.put("password", "1234567");
-        System.out.println(data);
-
-        JSONObject request = new JSONObject();
-        request.put("action", "login");
-        request.put("data", data);
-        System.out.println(request);
-
-        // Step 2: Convert the requsetJson object to string
-        // send it to the server
-        String jsonText = JSONValue.toJSONString(request);
-
-        // Step 3: in the server receive the jsonText and parse it to jsonObject
-        JSONObject jsonObject = (JSONObject) JSONValue.parse(jsonText);
-
-        // Step 4: fetch the data from the jsonObject
-        String action = (String) jsonObject.get("action");
-
-        // Note: data will be also json object and you have to use casting
-        JSONObject dataObject = (JSONObject) jsonObject.get("data");
-        String userName = (String) dataObject.get("username");
-        String password = (String) dataObject.get("password");
-
-        System.out.println("username: " + userName);
-        System.out.println("password: " + password);
+        return registerSuccessResponse();
     }
 }
--- TicTacToeGameServer/src/main/java/tictactoegameserver/Network/ResponseCreator.java
+++ TicTacToeGameServer/src/main/java/tictactoegameserver/Network/ResponseCreator.java
@@ -4,11 +4,14 @@
  */
 package tictactoegameserver.Network;
 
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
+import tictactoegameserver.Database.DatabaseManager;
+import tictactoegameserver.Database.Entities.Enums.MappingFunctions;
 import tictactoegameserver.Database.Entities.Player;
-import static tictactoegameserver.Network.RequestHandler.getOnlinePlayersJsonObject;
-import static tictactoegameserver.Network.RequestHandler.getPlayerJsonObject;
 
 /**
  *
@@ -32,10 +35,49 @@ public class ResponseCreator {
         responseObject.put("data", getPlayerJsonObject(userName, player));
         return JSONValue.toJSONString(responseObject);
     }
+        public static JSONObject getPlayerJsonObject(String userName, Player player) {
+        
+        JSONObject dataObject = new JSONObject();
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formatedDateTime = player.getRegisterDate().format(formatter);
+        dataObject.put("userName", player.getUserName());
+        dataObject.put("bonusPoints", player.getBonusPoints());
+        dataObject.put("playerRank", MappingFunctions.mapPlayerRank(player.getPlayerRank()));
+        dataObject.put("registerDate", formatedDateTime);
+        
+        return dataObject;
+    }
+    public static JSONObject getOnlinePlayersJsonObject() {
+        ArrayList<Player> onlinePlayers = null;
+        DatabaseManager.openDataBaseConnection();
+        onlinePlayers = DatabaseManager.getOnlinePlayers();
+        DatabaseManager.closeDataBaseConnection();
+        
+        JSONArray onlinePlayersNames = new JSONArray();
+        for (var player : onlinePlayers) {
+            onlinePlayersNames.add(player.getUserName());
+        }
+        JSONObject dataObject = new JSONObject();
+        dataObject.put("onlinePlayers", onlinePlayersNames);
+        
+        return dataObject;
+    }
     public static String updateOnlinePlayersResponse() {
         JSONObject responseObject = new JSONObject();
         responseObject.put("response", "update online players");
         responseObject.put("data", getOnlinePlayersJsonObject());
+        return JSONValue.toJSONString(responseObject);
+    }
+    
+    public static String playerExistResponse() {
+        JSONObject responseObject = new JSONObject();
+        responseObject.put("response", "player exists");
+        return JSONValue.toJSONString(responseObject);
+    }
+    public static String registerSuccessResponse() {
+        JSONObject responseObject = new JSONObject();
+        responseObject.put("response", "reqister sucsess");
         return JSONValue.toJSONString(responseObject);
     }
 
