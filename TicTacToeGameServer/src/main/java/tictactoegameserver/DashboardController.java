3389ea2a19f5edec4f6cfd7e95466b524f6c6cc2
reemadelsamak <72516521+reemadelsamak@users.noreply.github.com>
2022-02-17 17:53:40 +0200

    Chat View, Popups, Hover Fixed, Views Edited

---
+++ TicTacToeGameServer/src/main/java/tictactoegameserver/DashboardController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package tictactoegameserver;
+
+import java.awt.Button;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Roo
+ */
+public class DashboardController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    ImageView switchImage;
+    
+    @FXML
+    TableView playersTable;
+
+//    String onImagePath = "../../resources/images/on.png";
+//    String offImagePath = "../../resources/images/off.png";
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        playersTable.setPlaceholder(new Label("Open Server to Show Players"));
+        // TODO
+    }
+
+    @FXML
+    public void switchClicked() {
+//        if (server on)
+//         switchImage.setImage(new Image("../../resources/images/on.png"));
+
+//        if (server off)
+//         switchImage.setImage(new Image("../../resources/images/off.png"));
+    }
+
+}
---
+++ TickTackToeGame/src/main/java/com/main/ticktacktoegame/Controllers/AcceptOrDenyInvitationController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.main.ticktacktoegame.Controllers;
+
+import com.main.ticktacktoegame.App;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import com.main.ticktacktoegame.Network.Client;
+import com.main.ticktacktoegame.Network.RequestCreator;
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+/**
+ * FXML Controller class
+ *
+ * @author Roo
+ */
+public class AcceptOrDenyInvitationController {
+
+    @FXML
+    Label popupLabel;
+
+}
