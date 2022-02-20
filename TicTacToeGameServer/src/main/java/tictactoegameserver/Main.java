4827a3544d09efdffee23bbce9b88adba780ac70
Ayman Mohamed Hosny Kheir-Eldeen <Aymanmido067@gmail.com>
2022-02-20 02:17:07 +0200

    modifying project to generate jar file

---
+++ TicTacToeGameServer/src/main/java/tictactoegameserver/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tictactoegameserver;
+
+/**
+ *
+ * @author ayman
+ */
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
